d008806958203304fa2c9e262129a3920b198011
Edward Eisenberger <eeisenberger3@gmail.com>
2018-02-18 17:05:26 -0500

    Remove erroneous namespace

--- Thermostat/MainPage.xaml.cpp
+++ Thermostat/MainPage.xaml.cpp
@@ -24,7 +24,6 @@ using namespace Windows::Devices::Gpio;
 using namespace GNCommon;
 using namespace GNCommon::GNDrivers;
 
-using namespace 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 MainPage::MainPage()
