709aa5d5324615c0d33fb8551e69808175543506
Eisenberger, Edward <edward.eisenberger@live.com>
2021-12-23 15:41:27 -0500

    Added Home solution with Server project written in C Sharp

---
+++ Home/Server/MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Server
+{
+   using System;
+   using System.Collections.Generic;
+   using System.IO;
+   using System.Linq;
+   using System.Runtime.InteropServices.WindowsRuntime;
+   using Windows.Devices.Gpio;
+   using Windows.Foundation;
+   using Windows.Foundation.Collections;
+   using Windows.UI.Xaml;
+   using Windows.UI.Xaml.Controls;
+   using Windows.UI.Xaml.Controls.Primitives;
+   using Windows.UI.Xaml.Data;
+   using Windows.UI.Xaml.Input;
+   using Windows.UI.Xaml.Media;
+   using Windows.UI.Xaml.Navigation;
+
+   /// <summary>
+   /// An empty page that can be used on its own or navigated to within a Frame.
+   /// </summary>
+   public sealed partial class MainPage : Page
+   {
+      private const int LED_PIN = 5;
+      private GpioPin pin;
+      private GpioPinValue pinValue;
+      private DispatcherTimer timer;
+      private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
+      private SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.LightGray);
+      private Model.DHT sensor = new Model.DHT();
+
+      public MainPage( )
+      {
+         this.InitializeComponent( );
+
+         timer = new DispatcherTimer( );
+         timer.Interval = TimeSpan.FromMilliseconds( 5000 );
+         timer.Tick += Timer_Tick;
+         InitGPIO( );
+         if( pin != null )
+         {
+            timer.Start( );
+         }
+      }
+
+      private void InitGPIO( )
+      {
+         var gpio = GpioController.GetDefault();
+
+         // Show an error if there is no GPIO controller
+         if( gpio == null )
+         {
+            pin = null;
+            //GpioStatus.Text = "There is no GPIO controller on this device.";
+            return;
+         }
+
+         pin = gpio.OpenPin( LED_PIN );
+         pinValue = GpioPinValue.High;
+         pin.Write( pinValue );
+         pin.SetDriveMode( GpioPinDriveMode.Output );
+         sensor.Initialize( gpio.OpenPin( 6 ), gpio.OpenPin( 13 ) );
+      }
+
+      private void Timer_Tick( object sender, object e )
+      {
+         Model.DHT.Status status = sensor.Sample( );
+         txtDHTStatus.Text      = string.Format( "Status:      {0}", status );
+         txtDHTTemperature.Text = string.Format( "Temeprature: {0}", sensor.Temperature );
+         txtDHTHumidity.Text    = string.Format( "Humidity:    {0}", sensor.Humidity );
+
+         if( pinValue == GpioPinValue.High )
+         {
+            pinValue = GpioPinValue.Low;
+            pin.Write( pinValue );
+            //LED.Fill = redBrush;
+         }
+         else
+         {
+            pinValue = GpioPinValue.High;
+            pin.Write( pinValue );
+            //LED.Fill = grayBrush;
+         }
+      }
+   }
+}
---
+++ Home/Server/Model/DHT.cs
@@ -0,0 +1,122 @@
+﻿namespace Server.Model
+{
+   using System;
+   using System.Collections.Generic;
+   using System.Diagnostics;
+   using System.Linq;
+   using System.Text;
+   using System.Threading.Tasks;
+   using Windows.Devices.Gpio;
+
+   class DHT
+   {
+      public enum Status
+      {
+         None = 0,
+         StartHigh,
+         StartLow,
+         Timeout,     // Timed out waiting for sample
+         MissingData, // Did not catch all falling edges
+         InvalidData, // Checksum validation failed
+         Success
+      }
+
+      private const int   delayHoldLow  = 20;
+      private const int   delayHoldHigh = 40;
+      private const ulong timeout       = 1000; // 1000 Microseconds
+      private const ulong byteCount     = 5;
+
+      private GpioPin inputPin;
+      private GpioPin outputPin;
+      private GpioPinDriveMode inputDriveMode;
+      private GpioChangeReader changeReader;
+      private byte[] data;
+
+      public DHT( )
+      {
+         this.inputPin = null;
+         this.outputPin = null;
+         this.inputDriveMode = GpioPinDriveMode.Input;
+         this.data = new byte[ byteCount ];
+      }
+      public void Initialize( GpioPin aInputPin, GpioPin aOutputPin )
+      {
+         /// @par Process Design Language
+         /// -# Determine the input drive mode
+         ///   - Use InputPullUp if supported, otherwise fall back to Input (floating)
+         this.inputDriveMode = aInputPin.IsDriveModeSupported( GpioPinDriveMode.InputPullUp ) ?
+                               this.inputDriveMode = GpioPinDriveMode.InputPullUp : 
+                               this.inputDriveMode = GpioPinDriveMode.Input;
+         
+         /// -# Set the Input Pin Drive Mode and obtain Gpio Change Reader
+         this.inputPin = aInputPin;
+         this.inputPin.SetDriveMode( GpioPinDriveMode.Output );
+         this.changeReader = new GpioChangeReader( this.inputPin );
+
+         /// -# Set the Output Pin value to Low and Drive Mode to Output
+         this.outputPin.Write( GpioPinValue.Low );
+         this.outputPin.SetDriveMode( GpioPinDriveMode.Output );
+      }
+
+      public Status Sample( )
+      {
+         return( Status.None );
+      }
+
+      public bool IsValid
+      {
+         get
+         {
+            uint index;
+            uint checksum = 0;
+
+            for( index = 0; index < ( byteCount - 1 ); index++ )
+            {
+               checksum += data[ index ];
+            }
+
+            return ( ( checksum & 0x000000FF ) == data[ byteCount - 1 ] );
+         }
+      }
+
+      public double Humidity
+      {
+         get
+         {
+            return( ( double )data[ 0 ] + ( ( double )data[ 1 ] / 100.0 ) );
+         }
+      }
+
+      public double Temperature
+      {
+         get
+         {
+            return( ( double )data[ 2 ] + ( ( double )data[ 3 ] ) );
+         }
+      }
+
+      private bool sendInitialPulse( )
+      {
+         bool status = true;
+         Stopwatch timer = new Stopwatch( );
+         
+         /// @par Process Design Language
+         /// -# Bring the data line low for 18ms
+         this.outputPin.Write( GpioPinValue.Low );
+         timer.Start( );
+         while( timer.ElapsedMilliseconds < 18 )
+         {
+            ;
+         }
+         this.outputPin.Write( GpioPinValue.High );
+
+         /// -# Verify initial pulse is less than 30ms
+         if( timer.ElapsedMilliseconds > 30 )
+         {
+            status = false;
+         }
+
+         return( status );
+      }
+   }
+}
